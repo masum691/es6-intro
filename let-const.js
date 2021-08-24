9463371b578cee1a9a19ef08abf0886e2dd8bb94
masum691 <masumhu691@gmail.com>
2021-08-24 16:33:10 +0600

    let-const

---
+++ let-const.js
@@ -0,0 +1,25 @@
+
+const amount = 2334;
+let balance = 323;
+balance = 232;
+
+// not allowed
+// const number = [2323,342,3565,65];
+// number = [23,34,355];
+
+// can push, pop
+const num = [2323, 342, 3565, 65];
+num.pop();
+num.push(343);
+
+for (let i = 0; i < num.length; i++) {
+    const number = num[i];
+    sum = number + num;
+    
+}
+console.log('sum is', sum);
+
+const student = { roll: 34, name: 'Manik', job: 'at google' };
+student.name = ('Komola');
+console.log(student);
+
