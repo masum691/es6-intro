acbf241e2496c3a3b59c27d30a3631b9cba558f9
masum691 <masumhu691@gmail.com>
2021-08-24 17:09:29 +0600

    template string backtick

---
+++ template-string.js
@@ -0,0 +1,31 @@
+
+const love = 'I love my mother';
+const status = "I am a student";
+const intro = `I love my country. Bangladesh is a small countery`;
+console.log(love);
+console.log(status);
+console.log(intro);
+
+
+// backtick ` `
+const multiline = `Wow. are you really happy today?
+Are you married?
+Are you a student? Ow
+O
+K
+A
+Y
+`
+console.log(multiline);
+
+
+const friendCount = 6;
+const bestFriends = 2;
+const newOne = `I have ${friendCount} friends and ${bestFriends} best friends`;
+console.log(newOne)
+
+
+const first = 'Masum';
+const second = 'Hossain';
+const full = `My name is:  ${first} ${second}`;
+console.log(full);
