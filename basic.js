f889d50fd20f2840a2e1cd130cb75738bed734c3
masum691 <masumhu691@gmail.com>
2021-08-24 15:04:04 +0600

    basic js

---
+++ basic.js
@@ -0,0 +1,62 @@
+// variable
+var amount = 4533;
+
+// condition (compare)
+
+if(amount < 34422){
+
+}
+else if(amount > 4534){
+
+}
+else if(amount >= 3434){
+
+}
+else if(amount <= 3442){
+
+}
+else if(amount == 3334){
+
+}
+else if(amount != 534){
+
+}
+else{
+    console.log('unvalid number');
+}
+
+// array
+const numbers = [34,23,453,54,232,56,345,34];
+numbers.length;
+numbers.push(543);
+numbers.pop();
+numbers.unshift(545);
+numbers.shift();
+numbers[3] = 231;
+if(numbers.indexOf(543) != -1){
+
+}
+if(numbers.includes(342)){
+
+}
+
+// loop 
+// while loop , for loop , do....while loop
+for(const num of numbers){
+
+}
+
+// function
+function fullName (first, second){
+    const name = first + ' ' + second;
+}
+fullName('Masum', 'Hossain');
+
+
+// object 
+const laptop = {
+    name: 'acer',
+    color: 'black',
+    price: '38k'
+};
+
